b73a5024213d5f36dc92f2c8f1afcf2406731247
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-03 12:58:32 +0900

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,24 @@ use {
     std::fs::File,
 };
 
+macro_rules! dump {
+    ($node: expr, $file: expr) => {{
+        let node = $node;
+        node.write_as_gv(File::create($file).expect(""))
+            .expect("fail to serialize");
+        node
+    }};
+}
+
 fn main() {
-    // let stdout = io::stdout();
-    let idp_set: Node = example::independent_set();
-    assert_eq!(idp_set.len(), 127);
-    let f1 = File::create("ind-tree.gv").expect("fail to create");
-    idp_set.write_as_gv(f1).expect("fail to serialize");
-    let f2 = File::create("ind-bdd.gv").expect("fail to create");
-    BDD::new_from(idp_set)
-        .write_as_gv(f2)
-        .expect("fail to serialize");
-
-    let majority1: Node = example::majority();
-    let f3 = File::create("maj-tree.gv").expect("fail to create");
-    majority1.write_as_gv(f3).expect("fail to serialize");
-    let f4 = File::create("maj-bdd.gv").expect("fail to create");
-    BDD::new_from(majority1)
-        .write_as_gv(f4)
-        .expect("fail to serialize");
-
-    let x1x3: BDD<Node> = BDD::new_from(example::x1x3());
-    let x1x3f = File::create("x1x2-bdd.gv").expect("fail to create");
-    x1x3.write_as_gv(x1x3f).expect("fail to save");
-    let x2x3: BDD<Node> = BDD::new_from(example::x2x3());
-    let x2x3f = File::create("x2x3-bdd.gv").expect("fail to create");
-    x2x3.write_as_gv(x2x3f).expect("fail to save");
-    let applied: BDD<Node> = x1x3.apply(Box::new(|a, b| a | b), true, &x2x3);
-    let applyf = File::create("apply-bdd.gv").expect("fail to create");
-    applied.write_as_gv(applyf).expect("fail to save");
+    let idepend: Node = dump!(example::independent_set(), "ind-tree.gv");
+    dump!(BDD::new_from(idepend), "ind-bdd.gv");
+    let majority: Node = dump!(example::majority(), "maj-tree.gv");
+    dump!(BDD::new_from(majority), "maj-bdd.gv");
+    let x1x3: BDD<Node> = dump!(BDD::new_from(example::x1x3()), "x1x3-bdd.gv");
+    let x2x3: BDD<Node> = dump!(BDD::new_from(example::x2x3()), "x2x3-bdd.gv");
+    dump!(
+        x1x3.apply(Box::new(|a, b| a | b), true, &x2x3),
+        "apply-bdd.gv"
+    );
 }
